46020c2a837e8c48c184b3088a1775ccc30b43b7
MariiaDemidova <95860502+MariiaDemidova@users.noreply.github.com>
2021-12-13 00:47:13 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,24 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Homework_4
+namespace Homework_4._2._2
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.Write("N=");
-            int N = Convert.ToInt32(Console.ReadLine());
-            for (int Y = N / N; Y <= N; Y++)
+            static void Main(string[] args)
             {
-                int x = 0;
-                for (int j = 1; j <= Y; j++)
+                Console.WriteLine("Введите последовательность положительных и отрицательных чисел");
+                int X, Y;
+                X = Y = 0;
+                int N;
+                do
                 {
-                    x += (2 * j - 1);
-                }
-                Console.WriteLine("{0}", x);
+                    N = Convert.ToInt32(Console.Read());
+                    if (N > 0)
+                    {
+                        X++;
+                    }
+                    else
+                    {
+                        Y++;
+                    }
+                } while (N != 0);
+                Console.WriteLine("Количество положительных чисел - {0}, количество отрицательных - {1}", X, Y);
+                Console.ReadKey();
             }
-            Console.ReadKey();
-        }
     }
 }
